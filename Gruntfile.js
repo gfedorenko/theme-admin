433683631c1cc6e5365d146d463764637988eac3
Louis Bellet <lbellet@heliostech.fr>
2017-07-26 15:18:34 +0300

    Base structure

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files 'destination': 'source'
+          'library/assets/vendor/bootstrap/bootstrap.css': 'node_modules/bootstrap/scss/bootstrap.scss',
+          'library/assets/vendor/font-awesome/font-awesome.css': 'node_modules/font-awesome/scss/font-awesome.scss',
+          'library/assets/vendor/tether/tether.css': 'node_modules/tether/src/css/tether.sass'
+        }
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+        {expand: true, src: ['javascripts/**'], dest: 'library/assets/'},
+        {expand: true, cwd: 'node_modules/bootstrap/dist/js/', src: ['bootstrap.js'], dest: 'library/assets/vendor/bootstrap/', filter: 'isFile'},
+        {expand: true, cwd: 'node_modules/tether/dist/js/', src: ['tether.js'], dest: 'library/assets/vendor/tether/', filter: 'isFile'},
+        {expand: true, cwd: 'node_modules/jquery/dist/', src: ['jquery.js'], dest: 'library/assets/vendor/jquery/', filter: 'isFile'},
+        {expand: true, cwd: 'node_modules/font-awesome/', src: ['fonts/*'], dest: 'library/assets/vendor/font-awesome/', filter: 'isFile'}
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['sass', 'copy']);
+};
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # theme-admin
+
+## Getting started
+
+Require:
+* Ruby scss
+* nodejs
+* yarn
+* grunt-cli
+
+Enter the following commands:
+
+```
+yarn install
+node server.js
+```
